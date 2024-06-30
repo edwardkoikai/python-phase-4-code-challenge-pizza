ba353430e0f470d758843307d79dc98fb79ef392
edwardkoikai <edwardkoikai@gmail.com>
2024-07-01 00:35:26 +0300

    setup routes

--- server/app.py
+++ server/app.py
@@ -24,6 +24,82 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+@app.route('/restaurants')
+def restaurants():
+    restaurants = []
+    for restaurant in Restaurant.query.all():
+        # restaurant_dict = restaurant.to_dict()
+        restaurant_dict = {
+            "id": restaurant.id,
+            "name": restaurant.name,
+            "address": restaurant.address
+        }
+        restaurants.append(restaurant_dict)
+        
+    response = make_response(
+        restaurants,
+        200
+    )
+    
+    return response
+    
+@app.route('/restaurants/<int:id>', methods = ['GET', 'DELETE'])
+def restaurant_by_id(id):
+    restaurant =Restaurant.query.filter(Restaurant.id ==id).first()
+    
+    if request.method == 'GET':
+        if restaurant:
+            restaurant_dict = restaurant.to_dict()
+            response_body = restaurant_dict
+            status = 200
+        else:
+            response_body = {
+                "error": "Restaurant not found"
+            }
+            status = 404
+        return make_response(response_body, status)
+    
+    elif request.method == 'DELETE':
+        if restaurant:
+            db.session.delete(restaurant)
+            db.session.commit()
+            response_body = {
+                'delete_successful': True,
+                "message": "Restaurant deleted"
+            }
+            status = 204
+        else:
+            response_body = {
+                "error": "Restaurant not found"
+            }
+            status = 401
+            
+        response = make_response(response_body, status)
+        return response
+        
+@app.route('/pizzas')
+def pizzas():
+    pizzas = [] 
+    
+    for pizza in Pizza.query.all():
+        pizza_dict = {
+            "id": pizza.id,
+            "ingredients": pizza.ingredients,
+            "name": pizza.name
+        }
+        pizzas.append(pizza_dict)
+        
+    response = make_response(
+        pizzas, 
+        200
+    )
+    
+    return response
+        
+        
+
+    
+
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
